75794c3e89b71049c8e23ee1039fa55c18da0ff9
Haylie Goh <h.goh@deakin.edu.au>
2020-06-19 12:31:19 +1000

    changed to class component and finalised order by

--- src/App.js
+++ src/App.js
@@ -1,45 +1,46 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import EmployeeCard from "./components/EmployeeCard";
-import employees from "./employees.json";
+import employeesJSON from "./employees.json";
 
-function App() {
-  const [employeeState, setEmployeeState] = React.useState({
-    employees: employees,
-  });
-
-  const [startDate, setStartDate] = React.useState({});
-  const orderDate = employeeState.employees.sort((a, b) => b.date - a.date);
-
-  //   useEffect(() => {
-  //     console.log("render");
-  //   }, [values.password]);
-
-  function handleClick(e) {
-    e.preventDefault();
-    console.log("button clicked");
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      employeeState: employeesJSON,
+    };
   }
 
-  return (
-    <Wrapper>
-      <Title>Employee List</Title>
+  handleClick = (e) => {
+    e.preventDefault();
+    console.log("button clicked");
+    const employee = this.state.employeeState;
 
-      <button onClick={handleClick}>Order by Date</button>
+    for (var i = 0; i < employee.length; i++) {
+      // Convert startdate of each index into a Date object
+      const index = employee[i];
+      index.new_startdate = new Date(index.startdate);
+      // Sort employees array in ascending order
+      employee.sort((a, b) => (a.startdate > b.startdate ? 1 : -1));
+    }
+    this.setState({ employeeState: employee });
+  };
 
-      {employeeState.employees.map((employee, index) => (
-        <EmployeeCard
-          key={index}
-          name={employee.name}
-          image={employee.image}
-          title={employee.title}
-          startdate={employee.startdate}
-          telephone={employee.telephone}
-          email={employee.email}
-        />
-      ))}
-    </Wrapper>
-  );
+  render() {
+    const { employeeState } = this.state;
+    return (
+      <Wrapper>
+        <Title>Employee List</Title>
+        {/* Order cards */}
+        <button onClick={this.handleClick}>Order by Date</button>
+        {/* Render cards */}
+        {employeeState.map((employee, index) => {
+          return <EmployeeCard key={index} emp={employee} />;
+        })}
+        ;
+      </Wrapper>
+    );
+  }
 }
-
 export default App;
--- src/components/EmployeeCard/index.js
+++ src/components/EmployeeCard/index.js
@@ -2,27 +2,28 @@ import React from "react";
 import "./style.css";
 
 function FriendCard(props) {
+  const { emp } = props;
   return (
     <div className="card">
       <div className="img-container">
-        <img alt={props.name} src={props.image} />
+        <img alt={emp.name} src={emp.image} />
       </div>
       <div className="content">
         <ul>
           <li>
-            <strong>Name:</strong> {props.name}
+            <strong>Name:</strong> {emp.name}
           </li>
           <li>
-            <strong>Title:</strong> {props.title}
+            <strong>Title:</strong> {emp.title}
           </li>
           <li>
-            <strong>Start Date:</strong> {props.startdate}
+            <strong>Start Date:</strong> {emp.startdate}
           </li>
           <li>
-            <strong>Telephone:</strong> {props.telephone}
+            <strong>Telephone:</strong> {emp.telephone}
           </li>
           <li>
-            <strong>Email:</strong> {props.email}
+            <strong>Email:</strong> {emp.email}
           </li>
         </ul>
       </div>
