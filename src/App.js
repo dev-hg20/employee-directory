cc5738be036ae82e2eda127a4dd82a76084450d0
Haylie Goh <h.goh@deakin.edu.au>
2020-06-19 15:56:13 +1000

    start filtering

--- src/App.js
+++ src/App.js
@@ -27,11 +27,46 @@ class App extends React.Component {
     this.setState({ employeeState: employee });
   };
 
+  filterResults = (e) => {
+    e.preventDefault();
+    console.log("filter working");
+    const employee = this.state.employeeState;
+    console.log(employee); //ARRAY of objeccts
+    console.log(typeof employee); //object
+    employee.filter((item) => item.title === e.target.label);
+  };
+
   render() {
     const { employeeState } = this.state;
     return (
       <Wrapper>
         <Title>Employee List</Title>
+        {/* Filter cards */}
+        <div class="dropdown">
+          <button
+            class="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            Filter by Title
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            {/* TODO: Figure out event value */}
+            <a
+              label="Engineer"
+              class="dropdown-item"
+              onClick={this.filterResults}
+            >
+              Engineer
+            </a>
+            <a class="dropdown-item" onClick={this.filterResults}>
+              Test
+            </a>
+          </div>
+        </div>
         {/* Order cards */}
         <button onClick={this.handleClick}>Order by Date</button>
         {/* Render cards */}
